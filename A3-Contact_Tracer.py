8e6683e9ea941c81189620e6162dc523e4065811
John Kylle Santos <santosjohnkyllem@gmail.com>
2022-11-13 00:23:41 +0800

    added if else option and user option selector, dictionary name/detail adding code integrated in option 1, for searching the person will be integrated in option 2 for testing and debugging

--- A3-Contact_Tracer.py
+++ A3-Contact_Tracer.py
@@ -9,21 +9,29 @@
 
 contactPersons={}
 
-contactName=input("Please enter your full name: ")
-contactAge=int(input("Enter your age: "))
-contactNumber=int(input("Enter your contact number: "))
-contactAddress=input("Enter your present address: ")
-contactVax=input("Please state if you have been vaccinated already (Yes)/(No): ")
+userOption=int(input("Select your option: "))
 
-contactPersons[contactName]={}
-contactPersons[contactName]['Full name']=contactName
-contactPersons[contactName]['Age']=contactAge
-contactPersons[contactName]['Contact Number']=contactNumber
-contactPersons[contactName]['Present Address']=contactAddress
-contactPersons[contactName]['Vaccine Status']=contactVax
+if userOption=='1':
+    contactName=input("Please enter your full name: ")
+    contactAge=int(input("Enter your age: "))
+    contactNumber=int(input("Enter your contact number: "))
+    contactAddress=input("Enter your present address: ")
+    contactVax=input("Please state if you have been vaccinated already (Yes)/(No): ")
+
+    contactPersons[contactName]={}
+    contactPersons[contactName]['Full name']=contactName
+    contactPersons[contactName]['Age']=contactAge
+    contactPersons[contactName]['Contact Number']=contactNumber
+    contactPersons[contactName]['Present Address']=contactAddress
+    contactPersons[contactName]['Vaccine Status']=contactVax
+
+elif userOption=='2':
+    contactSearch=input("Enter the full name of your desired contact: ")
+    if contactSearch in contactPersons:
+        for key in contactPersons:
+            print(key,':',contactPersons[key])
 
 #for key, value in contactPersons:
 	#print(key, “ : ”, value)
 
-for key in contactPersons:
-    print(key,':',contactPersons[key])
+
